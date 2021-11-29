e9ca50cc809ad74051a9f8de06ca6b6a87ce1e86
Alexandria DAntonio <dantonio.alexandria@gmail.com>
2021-11-29 04:22:15 -0500

    commit tree acaa8bdcf74ecde9d111f3637939d8ddda5eb6f8

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+const returnFirstTwoDrivers = function(){
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(){
+    return drivers.slice(-2);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier){
+
+    //refactored
+    return fare => (fare * multiplier);
+
+    // return function(fare){
+    //     return fare * multiplier;
+    // }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, driverOrder = returnFirstTwoDrivers()){
+    
+    return driverOrder();
+}
